c92c0cafc7469d12a69a96bdab7f5961205a2221
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-05-22 09:28:58 +0300

    It will make the stopwatch function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+window.onload=function(){
+
 const displayEl=document.getElementById("display");
 const startEl= document.getElementById("startbtn");
 const stopEl= document.getElementById("stopbtn");
@@ -6,23 +8,42 @@ const secondEl= document.getElementById("sec");
 const microEl= document.getElementById("micro");
 var seconds=00;
 var micro=00;
+var interval;
 
-startEl.onclick= function(){
+startEl.onclick = function(){
     clearInterval(interval);
-    interval=setInterval(startTimer, 10);
-
+    interval=setInterval(startTimer,10)
 }
+
 stopEl.onclick= function(){
-    clearInterval(interval);
+        clearInterval(interval)
+
 }
-resetEl.onclick= function()
-{
-    clearInterval(interval)
+resetEl.onclick=function(){
+    clearInterval(interval);
     seconds="00";
     micro="00";
-    secondEl.innerHTML=seconds;
-    microEl.innerHTML=micro;
-
+    secondEl.innerHTML= seconds;  
+    microEl.innerHTML= micro; 
 }
+function startTimer(){
+    micro++;
+    if(micro <=9){
+        microEl.innerHTML= "0" + micro;
+    }
+    if(micro >9){
+        microEl.innerHTML=micro;
+    }
+    if(micro >99)
+    {
+        seconds++;
+        micro=0;
+        secondEl.innerHTML= "0"  + 0;
+        secondEl.innerHTML="0" + seconds;
+    }
+    if(seconds>9){
+        secondEl.innerHTML=seconds;
+    }
+}}
 
 
